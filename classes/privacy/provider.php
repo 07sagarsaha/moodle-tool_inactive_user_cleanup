cac7dc80a4cc64c99255c3afa54925299398a7f2
07sagarsaha <ss2749968@gmail.com>
2025-11-03 18:04:13 +0530

    Update provider.php

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -38,7 +38,9 @@ use core_privacy\local\request\transform;
  * @package   tool_inactive_user_cleanup
  */
 class provider implements
-    \core_privacy\local\metadata\provider, \core_privacy\local\request\core_userlist_provider, \core_privacy\local\request\plugin\provider {
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
+    \core_privacy\local\request\plugin\provider {
     /**
      * Describe data stored in the plugin's database tables.
      *
